d9f40003d592e5ca2dac408da7c34eb349e8775a
lablonczy <lukeablonczy@hotmail.com>
2020-02-16 17:43:33 -0500

    all classes to uml

--- Assignment.java
+++ Assignment.java
@@ -2,4 +2,19 @@ package CSCE247Iterator;//lukacs ablonczy
 
 public class Assignment {
 
+	private String title, description, topic;
+	private int points;
+
+	public Assignment(String title, String description, String topic, int points) {
+		this.title = title;
+		this.description = description;
+		this.topic = topic;
+		this.points = points;
+	}
+
+	public String toString() {
+		return "f";
+	}
+
+
 }
--- Course.java
+++ Course.java
@@ -2,4 +2,32 @@ package CSCE247Iterator;//lukacs ablonczy
 
 public class Course {
 
+	private Assignment[] assignments;
+	private int count;
+	private String name, title;
+
+	public Course(String name){
+		
+	}
+
+	public void addAssignment(String title, String description, String topic, int points){
+
+	}
+
+	public CourseIterator createIterator(){
+
+	}
+
+	public double getAssignmentWeights(){
+
+	}
+
+	public String toString(){
+
+	}
+
+	public Assignment[] growArray(Assignment[] first){
+
+	}
+
 }
