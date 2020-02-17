56c4ca835b0585c4f7ed228eff462852afb47580
lablonczy <lukeablonczy@hotmail.com>
2020-02-16 21:38:52 -0500

    All java done

--- Course.java
+++ Course.java
@@ -5,29 +5,40 @@ public class Course {
 	private Assignment[] assignments;
 	private int count;
 	private String name, title;
+	private static final int INITIAL_SIZE = 5, INITIAL_COUNT = 0, CONST_MULT = 2;
+	private static final double PERCENT = 100;
 
-	public Course(String name){
-
+	public Course(String name, String title) {
+		assignments = new Assignment[INITIAL_SIZE];
+		count = INITIAL_COUNT;
+		this.name = name;
+		this.title = title;
 	}
 
-	public void addAssignment(String title, String description, String topic, int points){
+	public void addAssignment(String title, String description, String topic, int points) {
+		if (count == assignments.length)
+			this.assignments = growArray(assignments);
 
+		assignments[count] = new Assignment(title, description, topic, points);
+		count++;
 	}
 
-	public CourseIterator createIterator(){
-
+	public CourseIterator createIterator() {
+		return new CourseIterator(assignments);
 	}
 
-	public double getAssignmentWeights(){
-
+	public double getAssignmentWeights() {
+		return PERCENT / assignments.length;
 	}
 
-	public String toString(){
-
+	public String toString() {
+		return name + ": " + title;
 	}
 
-	public Assignment[] growArray(Assignment[] first){
-
+	public Assignment[] growArray(Assignment[] first) {
+		Assignment[] newArray = new Assignment[assignments.length * CONST_MULT];
+		System.arraycopy(assignments, 0, newArray, 0, assignments.length);
+		return newArray;
 	}
 
 }
--- CourseIterator.java
+++ CourseIterator.java
@@ -7,16 +7,21 @@ public class CourseIterator implements Iterator {
 	private Assignment[] assignments;
 	private int position;
 
-	public CourseIterator(Assignment[] assignments){
-
+	public CourseIterator(Assignment[] assignments) {
+		this.assignments = assignments;
+		this.position = 0;
 	}
 
-	public boolean hasNext(){
-
+	public boolean hasNext() {
+		return position < assignments.length - 1;
 	}
 
-	public Assignment next(){
-
+	public Assignment next() {
+		if (position < assignments.length - 1) {
+			position++;
+			return assignments[position + 1];
+		} else
+			return null;
 	}
 
 }
